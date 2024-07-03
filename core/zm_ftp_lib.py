bce265f217544fc7df2ea3bead4aa75ce02d9055
boy07132004 <boy07132004@gmail.com>
2024-07-03 09:21:20 +0800

    add path check

--- core/zm_ftp_lib.py
+++ core/zm_ftp_lib.py
@@ -10,11 +10,34 @@ class ZM_FTP(FTP):
         passwd = config["FTP"]["FTP_PASSWD"]
         self.path = config["FTP"]["FOLDER_PATH"]
 
+        # remove the end slash
+        if len(self.path) > 0 and self.path[-1] == "/":
+            self.path = self.path[:-1]
+
         super().__init__(host)
         self.login(user=username, passwd=passwd)
+        self.rootPath = self.pwd()
 
-    def write_recipe(self, recipe, filename="default_name"):
-        _filename = self.path + '\\' + filename
+    def ftp_path_chk(self, remotePath=None):
+        self.cwd(self.rootPath)
+
+        if remotePath:
+            for i in range(1, len(remotePath.split("/"))):
+                path = remotePath.split("/")[i]
+                try:
+                    self.cwd(path)
+                except:
+                    self.mkd(path)
+                    self.cwd(path)
+
+        else:
+            print("Please input remote path.")
+
+    def write_recipe(self, recipe, filename="default_name.txt"):
+        _path =  self.rootPath + self.path 
+        self.ftp_path_chk(_path)
+        
+        _filename = _path + '/' + filename
         content_bytes = recipe.encode('utf-8')
 
         response = self.storbinary(
