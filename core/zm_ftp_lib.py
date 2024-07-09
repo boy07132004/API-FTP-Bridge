c21ad2ad678c833af3f7fed59d29c5901babca7f
boy07132004 <boy07132004@gmail.com>
2024-07-09 18:49:49 +0800

    remove path check

--- core/zm_ftp_lib.py
+++ core/zm_ftp_lib.py
@@ -7,41 +7,17 @@ class ZM_FTP(FTP):
         host = config["FTP"]["FTP_SERVER"]
         username = config["FTP"]["FTP_USER"]
         passwd = config["FTP"]["FTP_PASSWD"]
-        self.path = config["FTP"]["FOLDER_PATH"]
         self.logger = logger
-
-        # remove the end slash
-        if len(self.path) > 0 and self.path[-1] == "/":
-            self.path = self.path[:-1]
 
         super().__init__(host)
         self.login(user=username, passwd=passwd)
-        self.rootPath = self.pwd()
-
-    def ftp_path_chk(self, remotePath=None):
-        self.cwd(self.rootPath)
-
-        if remotePath:
-            for i in range(1, len(remotePath.split("/"))):
-                path = remotePath.split("/")[i]
-                try:
-                    self.cwd(path)
-                except:
-                    self.mkd(path)
-                    self.cwd(path)
-
-        else:
-            self.logger.warning("Please input remote path.")
 
     def write_recipe(self, recipe, filename="default_name.txt"):
-        _path = self.rootPath + self.path
-        self.ftp_path_chk(_path)
 
-        _filename = _path + '/' + filename
         content_bytes = recipe.encode('utf-8')
 
         response = self.storbinary(
-            'STOR ' + _filename, fp=io.BytesIO(content_bytes))
+            'STOR ' + filename, fp=io.BytesIO(content_bytes))
 
         if response.startswith('226'):
             self.logger.info("File uploaded successfully.")
